33a5ae10f3daf2518a048d01cc4f2fc0becd7768
Jammerjab1 <89883568+Jammerjab1@users.noreply.github.com>
2023-03-25 13:03:08 -0400

    Added functional Package and Customer

--- Assets/DetectCollision.cs
+++ Assets/DetectCollision.cs
@@ -4,13 +4,29 @@ using UnityEngine;
 
 public class DetectCollision : MonoBehaviour
 {
-   private void OnCollisionEnter2D(Collision2D collision)
-   {
-       Debug.Log("Wow " + collision.gameObject.name);
+    // variables
+    private bool hasPackage = false;
 
-   }
-   private void OnTriggerEnter2D(Collider2D trigger)
-   {
-       Debug.Log("Guh " + trigger.gameObject.name);
-   }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        //Debug.Log("Wow collision with" + collision.gameObject.name);
+
+    }
+    private void OnTriggerEnter2D(Collider2D trigger)
+    {
+        //Debug.Log("Guh trigger" + trigger.gameObject.name);
+
+        if(trigger.gameObject.CompareTag("Package") && !hasPackage)
+        {
+            hasPackage = true;
+            Debug.Log("Wow, a package!");
+            Destroy(trigger.gameObject, 0.5f);
+        }
+        if(trigger.gameObject.CompareTag("Customer") && hasPackage)
+        {
+            hasPackage = false;
+            Debug.Log("Delivered Package.");
+        }
+    }
 }
